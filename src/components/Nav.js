e9043744ef7d504dfc3b5d5c524e053f2edc7d59
Vivian Chen <chrysophyta@gmail.com>
2022-08-23 23:37:46 +0800

    clean up files

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <nav className="mx-8">
-      <ul className="flex h-16 bg-neutral-200 rounded-full space-between">
-        <li className="w-full h-full flex justify-center items-center">
-          <Link to="/">Home</Link>
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
         </li>
-        <li className="w-full h-full flex justify-center items-center">
-          <Link to="/foods">Food</Link>
+        <li>
+          <NavLink to="/foods">Food</NavLink>
         </li>
-        <li className="w-full h-full flex justify-center items-center">
-          <Link to="/stats">Stats</Link>
+        <li>
+          <NavLink to="/stats">Stats</NavLink>
         </li>
-        <li className="w-full h-full flex justify-center items-center">
-          <Link to="/stats">User</Link>
+        <li>
+          <NavLink to="/stats">User</NavLink>
         </li>
       </ul>
     </nav>
