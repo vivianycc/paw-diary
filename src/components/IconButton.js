f2eb2e137f38316c67a9e4d225b655dad55800d0
Vivian Chen <chrysophyta@gmail.com>
2022-11-10 01:18:30 +0800

    add variants

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -1,4 +1,32 @@
-import styled from "styled-components";
+import styled, { useTheme, css } from "styled-components";
+
+const variantStyles = (theme, variant = "primary") => {
+  return {
+    primary: css`
+      color: ${theme.colors.onPrimary};
+      background: ${theme.colors.primary};
+      border: 1px solid ${theme.colors.primary};
+    `,
+    secondary: css`
+      color: ${theme.colors.onSecondary};
+      background: ${theme.colors.secondary};
+      border: 1px solid ${theme.colors.secondary};
+    `,
+    outlined: css`
+      color: ${theme.colors.primary};
+      background: transparent;
+      border: 1px solid ${theme.colors.primary};
+    `,
+    transparent: css`
+      color: ${theme.colors.secondary};
+      background-color: transparent;
+      border: 1px solid transparent;
+      button &:hover {
+        background-color: var(--neutral-50);
+      }
+    `,
+  }[variant];
+};
 
 const StyledButton = styled.button`
   display: flex;
@@ -9,24 +37,24 @@ const StyledButton = styled.button`
   outline: none;
   border: none;
   border-radius: 100%;
-  background-color: ${(props) => props.color || "var(--neutral-700)"};
-  color: ${(props) => props.strokeColor || "var(--neutral-700)"};
+
+  ${({ theme, variant }) => variantStyles(theme, variant)}
 `;
 export default function IconButton({
   className,
   size = "48",
   onClick,
-  color = "var(--neutral-700)",
-  strokeColor = "#FFF",
   icon,
+  variant,
 }) {
+  const theme = useTheme();
   return (
     <StyledButton
-      className={className}
+      className={`${className || ""} icon-button`}
       size={size}
       onClick={onClick}
-      color={color}
-      strokeColor={strokeColor}
+      theme={theme}
+      variant={variant}
     >
       {icon}
     </StyledButton>
