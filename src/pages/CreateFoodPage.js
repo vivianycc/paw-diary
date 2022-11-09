cc26f59fa5674293ff0047c30c300537330c21c8
Vivian Chen <chrysophyta@gmail.com>
2022-11-09 19:38:49 +0800

    fix origin input

--- src/pages/CreateFoodPage.js
+++ src/pages/CreateFoodPage.js
@@ -11,11 +11,15 @@ import Button from "../components/Button";
 const { firestore } = getFirebase();
 const foodsCol = collection(firestore, "foods");
 
-const StyledForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  margin: 32px;
-  overflow: scroll;
+const StyledPage = styled.div`
+  height: 100vh;
+  padding: 32px;
+  overflow-y: scroll;
+  form {
+    display: flex;
+    flex-direction: column;
+    min-height: 0;
+  }
 
   label[for="ingredient"],
   label[for="foodType"] {
@@ -191,182 +195,171 @@ export default function CreateFoodPage(props) {
   };
 
   return (
-    <StyledForm onSubmit={handleSubmit} className="food-form">
-      <Input
-        label="品牌名稱"
-        placeholder="葛雷特"
-        name="brand"
-        value={foodInfo.brand}
-        onChange={handleChange}
-        required
-      />
-
-      <Input
-        label="系列名稱"
-        placeholder="精緻時光"
-        name="product"
-        value={foodInfo.product}
-        onChange={handleChange}
-      />
-
-      <Input
-        label="品項名稱"
-        placeholder="3號鮭魚＆火雞"
-        name="flavor"
-        value={foodInfo.flavor}
-        onChange={handleChange}
-      />
-
-      <Input
-        label="原產地"
-        placeholder="德國"
-        value={foodInfo.origin}
-        onChange={handleChange}
-      />
-
-      <div className="foodtype-select">
-        <label htmlFor="foodType"> 種類</label>
-        <Select
-          placeholder="主食"
-          name="foodType"
-          value={foodInfo.foodType}
-          onChange={handleFoodTypeSelect}
-        >
-          <Select.Option value="complete">主食</Select.Option>
-          <Select.Option value="complementary">副食</Select.Option>
-          <Select.Option value="treat">零食</Select.Option>
-          <Select.Option value="supplement">營養品</Select.Option>
-        </Select>
-      </div>
-
-      <Input
-        label="卡路里(kcal/100g)"
-        placeholder="191"
-        name="calories"
-        value={foodInfo.calories}
-        onChange={handleChange}
-      />
-
-      {/* {console.log("props", props)} */}
-      <div className="ingredient">
-        <label htmlFor="ingredient"> 主要原料</label>
-        <textarea
-          name="ingredient"
-          value={foodInfo.ingredient}
-          placeholder="雞肉,雞心...（請用逗點分開）"
+    <StyledPage>
+      <form onSubmit={handleSubmit} className="food-form">
+        <Input
+          label="品牌名稱"
+          placeholder="葛雷特"
+          name="brand"
+          value={foodInfo.brand}
+          onChange={handleChange}
+          required
+        />
+        <Input
+          label="系列名稱"
+          placeholder="精緻時光"
+          name="product"
+          value={foodInfo.product}
           onChange={handleChange}
         />
-      </div>
-
-      <Input
-        label="水份(%)"
-        name="water"
-        value={foodInfo.water}
-        onChange={handleChange}
-        placeholder="80.0"
-      />
-
-      <Input
-        label="蛋白質(%)"
-        name="protein"
-        value={foodInfo.protein}
-        onChange={handleChange}
-        placeholder="10.8"
-      />
-
-      <Input
-        label="脂肪(%)"
-        name="fat"
-        value={foodInfo.fat}
-        onChange={handleChange}
-        placeholder="5.6"
-      />
-
-      <Input
-        label="碳水化合物(%)"
-        name="carbonhydrate"
-        value={foodInfo.carbonhydrate}
-        onChange={handleChange}
-        placeholder="1.2"
-      />
-      <Input
-        label="灰質(%)"
-        name="ash"
-        value={foodInfo.ash}
-        onChange={handleChange}
-        placeholder="2.0"
-      />
-
-      <Input
-        label="纖維(%)"
-        name="fibre"
-        value={foodInfo.fibre}
-        onChange={handleChange}
-        placeholder="0.4"
-      />
-      <Input
-        label="鈣(%)"
-        name="calcium"
-        value={foodInfo.calcium}
-        onChange={handleChange}
-        placeholder="0.03"
-      />
-      <Input
-        label="磷(%)"
-        name="phosphorus"
-        value={foodInfo.phosphorus}
-        onChange={handleChange}
-        placeholder="0.03"
-      />
-      <Input
-        label="維他命 D3 (IE)"
-        name="vitd3"
-        value={foodInfo.vitd3}
-        onChange={handleChange}
-        placeholder="17"
-      />
-      <Input
-        label="維他命 E (mg)"
-        name="vite"
-        value={foodInfo.vite}
-        onChange={handleChange}
-        placeholder="0"
-      />
-      <Input
-        label="牛磺酸(mg)"
-        name="taurine"
-        value={foodInfo.taurine}
-        onChange={handleChange}
-        placeholder="127.5"
-      />
-
-      <Input
-        label="鋅(mg)"
-        name="zinc"
-        value={foodInfo.zinc}
-        onChange={handleChange}
-        placeholder="127.5"
-      />
-
-      <Input
-        label="錳(mg)"
-        name="manganese"
-        value={foodInfo.manganese}
-        onChange={handleChange}
-        placeholder="0.26"
-      />
-
-      <Input
-        label="碘(mg)"
-        name="iodine"
-        value={foodInfo.iodine}
-        onChange={handleChange}
-        placeholder="0.06"
-      />
-      <div className="buttons">
-        <Button label="返回上一頁" variant="secondary" />
-        <Button label="送出" type="submit" />
-      </div>
-    </StyledForm>
+        <Input
+          label="品項名稱"
+          placeholder="3號鮭魚＆火雞"
+          name="flavor"
+          value={foodInfo.flavor}
+          onChange={handleChange}
+        />
+        <Input
+          label="原產地"
+          placeholder="德國"
+          name="origin"
+          value={foodInfo.origin}
+          onChange={handleChange}
+        />
+        <div className="foodtype-select">
+          <label htmlFor="foodType"> 種類</label>
+          <Select
+            placeholder="主食"
+            name="foodType"
+            value={foodInfo.foodType}
+            onChange={handleFoodTypeSelect}
+          >
+            <Select.Option value="complete">主食</Select.Option>
+            <Select.Option value="complementary">副食</Select.Option>
+            <Select.Option value="treat">零食</Select.Option>
+            <Select.Option value="supplement">營養品</Select.Option>
+          </Select>
+        </div>
+        <Input
+          label="卡路里(kcal/100g)"
+          placeholder="191"
+          name="calories"
+          value={foodInfo.calories}
+          onChange={handleChange}
+        />
+        {/* {console.log("props", props)} */}
+        <div className="ingredient">
+          <label htmlFor="ingredient"> 主要原料</label>
+          <textarea
+            name="ingredient"
+            value={foodInfo.ingredient}
+            placeholder="雞肉,雞心...（請用逗點分開）"
+            onChange={handleChange}
+          />
+        </div>
+        <Input
+          label="水份(%)"
+          name="water"
+          value={foodInfo.water}
+          onChange={handleChange}
+          placeholder="80.0"
+        />
+        <Input
+          label="蛋白質(%)"
+          name="protein"
+          value={foodInfo.protein}
+          onChange={handleChange}
+          placeholder="10.8"
+        />
+        <Input
+          label="脂肪(%)"
+          name="fat"
+          value={foodInfo.fat}
+          onChange={handleChange}
+          placeholder="5.6"
+        />
+        <Input
+          label="碳水化合物(%)"
+          name="carbonhydrate"
+          value={foodInfo.carbonhydrate}
+          onChange={handleChange}
+          placeholder="1.2"
+        />
+        <Input
+          label="灰質(%)"
+          name="ash"
+          value={foodInfo.ash}
+          onChange={handleChange}
+          placeholder="2.0"
+        />
+        <Input
+          label="纖維(%)"
+          name="fibre"
+          value={foodInfo.fibre}
+          onChange={handleChange}
+          placeholder="0.4"
+        />
+        <Input
+          label="鈣(%)"
+          name="calcium"
+          value={foodInfo.calcium}
+          onChange={handleChange}
+          placeholder="0.03"
+        />
+        <Input
+          label="磷(%)"
+          name="phosphorus"
+          value={foodInfo.phosphorus}
+          onChange={handleChange}
+          placeholder="0.03"
+        />
+        <Input
+          label="維他命 D3 (IE)"
+          name="vitd3"
+          value={foodInfo.vitd3}
+          onChange={handleChange}
+          placeholder="17"
+        />
+        <Input
+          label="維他命 E (mg)"
+          name="vite"
+          value={foodInfo.vite}
+          onChange={handleChange}
+          placeholder="0"
+        />
+        <Input
+          label="牛磺酸(mg)"
+          name="taurine"
+          value={foodInfo.taurine}
+          onChange={handleChange}
+          placeholder="127.5"
+        />
+        <Input
+          label="鋅(mg)"
+          name="zinc"
+          value={foodInfo.zinc}
+          onChange={handleChange}
+          placeholder="127.5"
+        />
+        <Input
+          label="錳(mg)"
+          name="manganese"
+          value={foodInfo.manganese}
+          onChange={handleChange}
+          placeholder="0.26"
+        />
+        <Input
+          label="碘(mg)"
+          name="iodine"
+          value={foodInfo.iodine}
+          onChange={handleChange}
+          placeholder="0.06"
+        />
+        <Button.Group>
+          <Button label="返回上一頁" variant="secondary" />
+          <Button label="送出" type="submit" />
+        </Button.Group>
+      </form>
+    </StyledPage>
   );
 }
